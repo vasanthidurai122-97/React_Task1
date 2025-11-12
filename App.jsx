0f4886687fb507e9a171ca738ce3f4e94b42b31e
vasanthidurai122_97 <61723599+vasanthidurai122-97@users.noreply.github.com>
2025-11-12 11:44:39 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom'
 import Home from './assets/components/Home'
 import Toggle from './assets/components/Toggle'
 import ProductList from './assets/components/ProductList'
@@ -10,9 +11,15 @@ function App() {
 
   return (
     <>
-      <Home/>
-      <Toggle/>
-      <ProductList/>
+       <Router>
+      <Header/>
+      <Routes>
+           <Route path="/" element={<Home/>} />
+           <Route path="/Toggle" element ={<Toggle/>}/>
+           <Route path = "/ProductList" element = {<ProductList/>}/>
+      
+      </Routes>
+      </Router>
     </>
   )
 }
